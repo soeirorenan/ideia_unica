fd5a3d023f60f2569f459ade85658741d6173638
Renan Soeiro <renan.ifce@gmail.com>
2021-03-08 00:57:44 -0300

    stale-while-revalidate

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,6 +1,8 @@
 function tempo(request, response){
     const dynamicDate = new Date();
 
+    response.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate');
+
     response.json({
         date: dynamicDate.toGMTString()
     })
